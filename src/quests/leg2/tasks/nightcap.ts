9c93fedeaecf88146d04b15a7b66cc0239df4131
Joshua Dietrich <jdietrich@ascentos.ai>
2024-01-19 10:15:59 -0800

    Fixed nightcap task not being run Set version number to 1.0.0

--- src/quests/leg2/tasks/nightcap.ts
+++ src/quests/leg2/tasks/nightcap.ts
@@ -1,8 +1,12 @@
 import { Task } from "grimoire-kolmafia";
 import { cliExecute, inebrietyLimit, myInebriety } from "kolmafia";
+import { $familiar } from "libram";
 
 export const NIGHTCAP: Task = {
   name: "Nightcap",
-  completed: () => myInebriety() > inebrietyLimit(),
+  outfit: {
+    familiar: $familiar`Stooper`,
+  },
+  completed: () => myInebriety() > inebrietyLimit() + 1,
   do: () => cliExecute("CONSUME ALL NIGHTCAP"),
 };
